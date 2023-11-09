7124fad33fe078123b11ac6b662953147bbfae67
ChrisuTheWeak <Christio@metropolia.fi>
2023-11-09 16:39:30 +0200

    Initial Commit test

---
+++ RaflaFinder/RaflaFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaflaFinderApp.swift
+//  RaflaFinder
+//
+//  Created by iosdev on 9.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct RaflaFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
