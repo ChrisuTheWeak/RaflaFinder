54441bc16c09aeca56f890d097572d895a4fd594
ChrisuTheWeak <Christio@metropolia.fi>
2023-11-13 15:26:33 +0200

    monkey feet

--- RaflaFinder/RaflaFinderApp.swift
+++ RaflaFinder/RaflaFinderApp.swift
@@ -6,9 +6,11 @@
 //
 
 import SwiftUI
+import MapKit
 
 @main
 struct RaflaFinderApp: App {
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
     var body: some Scene {
         WindowGroup {
             ContentView()
