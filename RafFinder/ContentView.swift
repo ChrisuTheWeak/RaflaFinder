d655a8e2d5d7fc8926b8068ec11f555db2e8d434
ChrisuTheWeak <Christio@metropolia.fi>
2023-11-15 15:28:59 +0200

    Added Map View

--- RafFinder/ContentView.swift
+++ RafFinder/ContentView.swift
@@ -6,21 +6,31 @@
 //
 
 import SwiftUI
+import MapKit
+import CoreLocation
+
 
 struct ContentView: View {
+    let locationManager = CLLocationManager()
+    
+    @State var region = MKCoordinateRegion(
+        center: .init(latitude: 60.223977,longitude: 24.758622),
+        span: .init(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Map(coordinateRegion: $region,
+            showsUserLocation: true,
+            userTrackingMode: .constant(.follow)
+        ).edgesIgnoringSafeArea(.all)
+            .onAppear {
+                locationManager.requestWhenInUseAuthorization()
+            }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
