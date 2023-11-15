8bacc3c9aceef85736940170045ec9c0dbbefdcd
ChrisuTheWeak <Christio@metropolia.fi>
2023-11-15 15:18:24 +0200

    Initial Commit

---
+++ RafFinder/RafFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RafFinderApp.swift
+//  RafFinder
+//
+//  Created by iosdev on 15.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct RafFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
