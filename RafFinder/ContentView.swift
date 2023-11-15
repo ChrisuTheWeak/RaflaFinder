15c748aa709da97c614ade4a07b9e9b854a26039
RiQhy <riku.nokelainen@metropolia.fi>
2023-11-15 15:39:31 +0200

    First view on Menu

--- RafFinder/ContentView.swift
+++ RafFinder/ContentView.swift
@@ -1,24 +1,61 @@
 //
 //  ContentView.swift
-//  RafFinder
+//  RaflaFinder
 //
-//  Created by iosdev on 15.11.2023.
+//  Created by iosdev on 11.11.2023.
 //
 
 import SwiftUI
+import WebKit
+
 
 struct ContentView: View {
+    @State private var userInput: String = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            VStack {
+                TextField("Enter food or restaurant name", text: $userInput)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .foregroundColor(.blue)
+                    .padding(.horizontal)
+                    .overlay(
+                        HStack{
+                            Spacer()
+                            Button(action: {
+                                
+                            }) {
+                                Image(systemName: "mic.fill")
+                                    .foregroundColor(.blue)
+                            }
+                            .padding(.trailing, 50)
+                        }
+                    )
+                
+                Text("Restaurant Name Here")
+                    .bold()
+                    .fontWeight(.light)
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 200)
+                    .padding(.bottom)
+                
+                Text("Menu")
+                    .font(.title2)
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0)
+                    .padding(.bottom)
+                
+                Link(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=URL@*/URL(string: "https://www.apple.com")!/*@END_MENU_TOKEN@*/) {
+                    Rectangle()
+                        .fill(.yellow)
+                        .frame(minWidth: 0, maxWidth: 380, minHeight:0)
+                }
+            }
+            .offset(y:0)
+            .frame(minWidth: 0, maxHeight: .infinity, alignment: .topLeading)
         }
-        .padding()
+        
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
