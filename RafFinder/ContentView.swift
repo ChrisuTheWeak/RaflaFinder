afe43e6ad390a886ab67036b51489409845ded5d
Semen Morozov <semenmorozovsuomi@gmail.com>
2023-11-16 13:58:32 +0200

    front page updates

--- RafFinder/ContentView.swift
+++ RafFinder/ContentView.swift
@@ -3,19 +3,17 @@
 //  RafFinder
 //
 //  Created by iosdev on 15.11.2023.
-//
+//  Semen Morozov
+
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            FrontScreenView()
+                .navigationBarHidden(true)
         }
-        .padding()
     }
 }
 
@@ -24,3 +22,85 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct FrontScreenView: View {
+    @State private var userInput: String = ""
+
+    var body: some View {
+        ZStack {
+            VStack {
+                HStack {
+                    Text("Rafla Finder")
+                        .font(.title)
+                        .fontWeight(.heavy)
+                        .multilineTextAlignment(.center)
+                        .kerning(1.4)
+                        .padding()
+                        .foregroundColor(.blue)
+                }
+
+                Spacer(minLength: 0)
+
+                Image("logoImage")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.horizontal, 16)
+                    .frame(height: 300)
+
+                Spacer(minLength: 80)
+
+                TextField("Enter food or restaurant name", text: $userInput)
+                    .padding()
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .foregroundColor(.blue)
+                    .frame(height: 100)
+                    .padding(.horizontal)
+                    .overlay(
+                        HStack {
+                            Spacer()
+                            Button(action: {
+                           
+                            }) {
+                                Image(systemName: "mic.fill")
+                                    .foregroundColor(.blue)
+                            }
+                            .padding(.trailing, 50)
+                        }
+                    )
+
+                Spacer()
+
+                Button(action: {
+                
+                }) {
+                    Text("Random")
+                        .fontWeight(.heavy)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.gray)
+                        .cornerRadius(40)
+                        .padding(.horizontal, 16)
+                
+                }
+
+                Button(action: {
+                    
+                }) {
+                    Text("Let's Go")
+                        .fontWeight(.heavy)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(40)
+                        .padding(.horizontal, 16)
+                    
+                }
+            }
+        }
+        .padding(.bottom, 80)
+        .background(Color.white)
+    }
+}
+
